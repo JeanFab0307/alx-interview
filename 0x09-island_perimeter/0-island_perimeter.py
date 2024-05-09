ea69ce27396fc8326a59c5139b43bfd7d3a88187
JeanFab0307 <jfrandriatsimivony@gamil.com>
2024-05-09 23:33:06 +0300

    Apply PEP8 style

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,17 +3,18 @@
 island_perimeter function
 """
 
+
 def island_perimeter(grid):
-  """ returns the perimeter of the island described in grid"""
-  perimeter = 0
-  first = True
-  for i in range(len(grid)):
-    for j in range(len(grid[i])):
-      if grid[i][j] == 1:
-        perimeter += 4 if first else 3
-        first = False
-        if grid[i - 1][j] and grid[i - 1][j] == 1:
-          perimeter -= 1
-        if grid[i][j - 1] and grid[i][j - 1] == 1:
-          perimeter -= 1
-  return perimeter
+    """ returns the perimeter of the island described in grid"""
+    perimeter = 0
+    first = True
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4 if first else 3
+                first = False
+                if grid[i - 1][j] and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if grid[i][j - 1] and grid[i][j - 1] == 1:
+                    perimeter -= 1
+    return perimeter
