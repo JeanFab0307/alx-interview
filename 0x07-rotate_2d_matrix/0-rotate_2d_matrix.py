f80a5ca4da4f434873c3ab73a9d512f171882153
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-09-12 21:39:00 +0300

    Apply pip8 codestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""
-Rotate 2D Matrix
-"""
+"""Rotate 2D Matrix."""
 
 
 def rotate_2d_matrix(matrix):
-	"""
-		Takes  matrix and rotate it clockwise
-		Return:
-		Nothing, the matrix is edited in place
-	"""
-	for x in range(0, int(len(matrix)/2)):
-		for y in range(x ,len(matrix) - x - 1):
-			temp = matrix[x][y]
+    """
+    Takes matrix and rotate it clockwise
+    Return:
+    Nothing, the matrix is edited in place
+    """
+    size = len(matrix)
+    for x in range(0, int(size/2)):
+        for y in range(x, size - x - 1):
+            temp = matrix[x][y]
 
-			matrix[x][y] = matrix[len(matrix) - 1 - y][x]
+            matrix[x][y] = matrix[size - 1 - y][x]
 
-			matrix[len(matrix) - 1 - y][x] = matrix[len(matrix) - 1 - x][len(matrix) - 1 - y]
+            matrix[size - 1 - y][x] = matrix[size - 1 - x][size - 1 - y]
 
-			matrix[len(matrix) - 1 - x][len(matrix) - 1 - y] = matrix[y][len(matrix) - 1 - x]
+            matrix[size - 1 - x][size - 1 - y] = matrix[y][size - 1 - x]
 
-			matrix[y][len(matrix) - 1 - x] = temp
-			
+            matrix[y][size - 1 - x] = temp
