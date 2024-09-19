295d37734a1546cb6d704850a20383bc92bc6e3a
JeanFab0307 <jfrandriatsimivony@gmail.com>
2024-09-19 16:31:55 +0300

    Add making change interview task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""make change"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return:
+    fewer number of coins needed to meet total
+    Or 0 is total <= 0
+    Or -1 if not possible
+    """
+    i = 0
+    memo = total
+    while total > 0:
+        i += 1
+        for coin in coins:
+            memo1 = total - coin
+            if memo1 < memo and memo1 >=0 :
+                memo = memo1
+        if total == memo :
+            return -1
+        total = memo
+    if total <= 0:
+        return i
+
+    
+ 
